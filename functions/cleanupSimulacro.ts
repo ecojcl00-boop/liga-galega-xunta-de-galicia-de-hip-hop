ea2cda1692602c2cec437ca30b1e7ec42ffd2a90
Javier Ignacio Cabanas Lopez <ecojcl00@gmail.com>
2026-03-11 10:33:08 +0000

    File changes

--- functions/cleanupSimulacro.ts
+++ functions/cleanupSimulacro.ts
@@ -17,17 +17,19 @@ Deno.serve(async (req) => {
       return Response.json({ error: 'Forbidden: Admin access required' }, { status: 403 });
     }
 
-    const [regs, ligaResultados, ligaComps] = await Promise.all([
+    const [regs, ligaResultados, ligaComps, groups] = await Promise.all([
       base44.asServiceRole.entities.Registration.filter({ is_simulacro: true }, "-created_date", 1000),
       base44.asServiceRole.entities.LigaResultado.filter({ is_simulacro: true }, "-created_date", 2000),
       base44.asServiceRole.entities.LigaCompeticion.filter({ is_simulacro: true }, "-created_date", 100),
+      base44.asServiceRole.entities.Group.filter({ is_simulacro: true }, "-created_date", 1000),
     ]);
 
-    console.log(`[INFO] Cleanup simulacro: ${regs.length} regs, ${ligaResultados.length} resultados, ${ligaComps.length} comps`);
+    console.log(`[INFO] Cleanup simulacro: ${regs.length} regs, ${ligaResultados.length} resultados, ${ligaComps.length} comps, ${groups.length} grupos`);
 
     await deleteAll(base44.asServiceRole.entities.Registration, regs);
     await deleteAll(base44.asServiceRole.entities.LigaResultado, ligaResultados);
     await deleteAll(base44.asServiceRole.entities.LigaCompeticion, ligaComps);
+    await deleteAll(base44.asServiceRole.entities.Group, groups);
 
     console.log(`[INFO] Cleanup done`);
     return Response.json({
@@ -36,6 +38,7 @@ Deno.serve(async (req) => {
         registrations: regs.length,
         liga_resultados: ligaResultados.length,
         liga_competiciones: ligaComps.length,
+        groups: groups.length,
       }
     });
   } catch (error) {
